52fbb68b79c91791d9c85152d1393b136249698e
Melvin Jones Repol <mrepol742@gmail.com>
2024-08-14 04:25:36 +0800

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
