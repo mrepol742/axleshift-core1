f9401a7588734ebe5c30ff1650861208abb3e049
Melvin Jones Repol <mrepol742@gmail.com>
2024-11-16 00:29:57 +0800

    refractor security management panels

--- client/src/views/security/panel/index.jsx
+++ client/src/views/security/panel/index.jsx
@@ -1,9 +1,9 @@
-import Activity from './activity'
-import Api from './api'
-import Dashboard from './dashboard'
-import Dependabot from './dependabot'
-import Maintenance from './maintenance'
-import Sentry from './sentry'
-import Sessions from './sessions'
+import Activity from './Activity'
+import Api from './API'
+import Dashboard from './Dashboard'
+import Dependabot from './Dependabot'
+import Maintenance from './Maintenance'
+import Sentry from './Sentry'
+import Sessions from './Sessions'
 
 export { Activity, Api, Dashboard, Dependabot, Maintenance, Sentry, Sessions }
