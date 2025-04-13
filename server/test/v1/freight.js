ac5a72a81d1e223b1c82177faadbb1d034429f6a
Melvin Jones Repol <mrepol742@gmail.com>
2025-04-14 07:38:52 +0800

    bug fixes

--- server/test/v1/freight.js
+++
@@ -1,9 +0,0 @@
-import request from 'supertest'
-import { expect } from 'chai'
-import app from '../../src/Server.js'
-
-const TOKEN = 'af1b6864fa4280ec8f70ac01e5533e39b457c26fcef2b4e9ff53b240b451b94a'
-
-const freight = () => {}
-
-export default freight
