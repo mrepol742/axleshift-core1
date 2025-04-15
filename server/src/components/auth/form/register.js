a1d55119d58eaa5c01e90a73ea3813ed0c9a08b1
Melvin Jones Repol <mrepol742@gmail.com>
2025-04-15 14:47:21 +0000

    address issues with newsletter

--- server/src/components/auth/form/register.js
+++ server/src/components/auth/form/register.js
@@ -4,12 +4,14 @@ import logger from '../../../utils/logger.js'
 import { send } from '../../mail.js'
 import activity, { sendNotification } from '../../activity.js'
 import { APP_KEY, NODE_ENV } from '../../../config.js'
+import addToNewsletter from '../../../components/newsletter.js'
 
 const FormRegister = async (req, res) => {
     try {
         const { username, email, first_name, last_name, password, newsletter } = req.body
         const db = await database()
         const usersCollection = db.collection('users')
+
         const existingUser = await usersCollection.findOne({
             $or: [
                 { [`oauth2.google.email`]: email },
@@ -40,6 +42,7 @@ const FormRegister = async (req, res) => {
         const dateNow = Date.now()
 
         await Promise.all([
+            addToNewsletter(email),
             usersCollection.insertOne({
                 email: email,
                 first_name: first_name,
---
+++ server/src/components/newsletter.js
@@ -0,0 +1,20 @@
+import database from '../models/mongodb.js'
+
+const newsletter = async (email) => {
+  const db = await database()
+        const newsletterCollection = db.collection('newsletter')
+        const existingSubscriber = await newsletterCollection.findOne({ email: email })
+        if (existingSubscriber)
+            return true
+
+        const dateNow = Date.now()
+        await newsletterCollection.insertOne({
+            email: email,
+            is_subsribe: true,
+            created_at: dateNow,
+            updated_at: dateNow,
+        })
+        return false
+}
+
+export default newsletter
