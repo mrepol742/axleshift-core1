781128e2f25caf88cfb79cd59d887bf8f57d1f60
Melvin Jones Repol <mrepol742@gmail.com>
2025-04-16 09:28:10 +0800

    improve pm2 ecosystem config

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -3,9 +3,16 @@ module.exports = {
         {
             name: 'core1-axleshift',
             script: 'src/index.js',
-            instances: 'max',
+            instances: 3,
             exec_mode: 'cluster',
             sticky: true,
+            node_args: "--max-old-space-size=1024",
+            env: {
+                NODE_ENV: 'production',
+            },
+            max_memory_restart: '512M',
+            watch: false,
+            autorestart: true,
         },
     ],
 }
