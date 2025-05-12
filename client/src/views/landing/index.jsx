e4d88b424032678f5c275922cf9edd56fd18ec0c
Melvin Jones Repol <mrepol742@gmail.com>
2025-05-12 13:44:27 +0800

    add environment check for redirect in Landing component

--- client/src/views/landing/index.jsx
+++ client/src/views/landing/index.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { CSpinner } from '@coreui/react'
-import { VITE_APP_SESSION } from '../../config'
+import { VITE_APP_SESSION, VITE_APP_NODE_ENV } from '../../config'
 
 const Landing = () => {
     const navigate = useNavigate()
 
     useEffect(() => {
         if (cookies.get(VITE_APP_SESSION)) return navigate('/dashboard')
-        window.location.href = 'https://axleshift.com'
+        if (VITE_APP_NODE_ENV !== 'development') window.location.href = 'https://axleshift.com'
+        window.location.href = '/login'
     }, [])
 
     return (
